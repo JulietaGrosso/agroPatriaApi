db68b8d88808b39efbea1815b96c98d00f1aaac4
Julieta <julj.grosso16@gmail.com>
2025-09-27 21:58:00 -0300

    avances

---
+++ agropatriaapi/src/main/java/com/agropatriaapp/agropatriaapi/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.agropatriaapp.agropatriaapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all paths
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://julietagrosso.github.io") // Specify allowed origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                .allowedHeaders("*") // Allowed headers
+                .allowCredentials(true) // Allow sending cookies/auth headers
+                .maxAge(3600); // Cache preflight response for 1 hour
+    }
+}
