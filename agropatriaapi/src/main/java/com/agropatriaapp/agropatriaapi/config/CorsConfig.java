0f9c80880027982234f42763d2a50514c0a6c67f
Julieta <julj.grosso16@gmail.com>
2025-10-21 11:52:17 -0300

    fix: fix cors

--- agropatriaapi/src/main/java/com/agropatriaapp/agropatriaapi/config/CorsConfig.java
+++ agropatriaapi/src/main/java/com/agropatriaapp/agropatriaapi/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all paths
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://julietagrosso.github.io", "https://agropatria.com", "https://www.agropatria.com") // Specify allowed origins
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "https://julietagrosso.github.io", "https://agropatria.com", "https://www.agropatria.com", "https://webapp.agropatria.com") // Specify allowed origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allowed headers
                 .allowCredentials(true) // Allow sending cookies/auth headers
